4d4e3ef22418c33b081228eced9b19a77df33885
Achyudh Ram <achyudhk@users.noreply.github.com>
2017-06-14 20:00:39 +0530

    Update Auth

--- app/src/main/java/com/example/rkumar32/foresight/DetailActivity.java
+++ app/src/main/java/com/example/rkumar32/foresight/DetailActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.rkumar32.foresight.utility.PlaceWrapper;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -19,5 +21,10 @@ public class DetailActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.place_id)).setText("Place ID: " + placeWrapper.getId());
         ((TextView) findViewById(R.id.name)).setText(placeWrapper.getName());
         ((TextView) findViewById(R.id.rating)).setText("Rating: " + placeWrapper.getRating() + "");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+
     }
 }
--- app/src/main/java/com/example/rkumar32/foresight/MainActivity.java
+++ app/src/main/java/com/example/rkumar32/foresight/MainActivity.java
@@ -1,34 +1,64 @@
 package com.example.rkumar32.foresight;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rkumar32.foresight.utility.PlaceWrapper;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
     public static final int PLACE_PICKER_REQUEST = 1;
+    public static final int RC_SIGN_IN = 2;
     public static final String PLACE_KEY = "place";
-
+    public GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
     private Button mButton;
+    private SignInButton signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mButton = (Button) findViewById(R.id.button);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mAuth = FirebaseAuth.getInstance();
+        mButton = (Button) findViewById(R.id.button_picker);
         mButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         // This launches the place picker
                         try {
-                            PlacePicker.IntentBuilder  builder = new PlacePicker.IntentBuilder();
+                            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
                             startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
                         } catch (Exception e) {
 
@@ -36,7 +66,22 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+        signInButton = (SignInButton) findViewById(R.id.button_login);
+        signInButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        switch (v.getId()) {
+                            case R.id.button_login:
+                                signIn();
+                                break;
+                    }
+                }});
+    }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -49,5 +94,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+        }
+
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
